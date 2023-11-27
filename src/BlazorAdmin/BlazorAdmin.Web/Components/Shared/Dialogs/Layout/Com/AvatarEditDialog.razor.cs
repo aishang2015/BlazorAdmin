006740a91aeaaaa1902d99f1cd58d6e7339a0886
aishang2015 <zhang__tian__hua@126.com>
2023-11-27 17:11:40 +0800

    delete old avatar file

--- src/BlazorAdmin/BlazorAdmin.Web/Components/Shared/Dialogs/Layout/Com/AvatarEditDialog.razor.cs
+++ src/BlazorAdmin/BlazorAdmin.Web/Components/Shared/Dialogs/Layout/Com/AvatarEditDialog.razor.cs
@@ -10,112 +10,110 @@ using System.Text.RegularExpressions;
 
 namespace BlazorAdmin.Web.Components.Shared.Dialogs.Layout.Com
 {
-    public partial class AvatarEditDialog
-    {
-        [Parameter] public IBrowserFile? BrowserFile { get; set; }
+	public partial class AvatarEditDialog
+	{
+		[Parameter] public IBrowserFile? BrowserFile { get; set; }
 		[CascadingParameter] MudDialogInstance? MudDialog { get; set; }
 
+		private string src = "";
 
-		private readonly string _errorLoadImageSrc = "not-found-image.jpg";
-        private bool IsErrorLoadImage { get; set; } = false;
-        private bool IsAvailableInitCropper { get; set; } = true;
-        private string src = "";
-
-        private Options _options = new Options
-        {
-            AspectRatio = 1,
-            ViewMode = ViewMode.Vm1,
-        };
-        private CropperComponent? cropperComponent;
-        private ElementReference ElementReference;
+		private Options _options = new Options
+		{
+			AspectRatio = 1,
+			ViewMode = ViewMode.Vm1,
+		};
+		private CropperComponent? cropperComponent;
+		private ElementReference ElementReference;
 
 
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            await base.OnAfterRenderAsync(firstRender);
-            if (firstRender)
-            {
-                _options.Preview = new ElementReference[]
-                {
-                    ElementReference
-                };
+		protected override async Task OnAfterRenderAsync(bool firstRender)
+		{
+			await base.OnAfterRenderAsync(firstRender);
+			if (firstRender)
+			{
+				_options.Preview = new ElementReference[]
+				{
+					ElementReference
+				};
 
-                if (BrowserFile != null && cropperComponent != null)
-                {
-                    string oldSrc = src;
+				if (BrowserFile != null && cropperComponent != null)
+				{
+					string oldSrc = src;
 
-                    src = await cropperComponent.GetImageUsingStreamingAsync(BrowserFile, BrowserFile.Size);
+					src = await cropperComponent.GetImageUsingStreamingAsync(BrowserFile, BrowserFile.Size);
 
-                    IsAvailableInitCropper = true;
-                    IsErrorLoadImage = false;
 
-                    cropperComponent?.Destroy();
-                    cropperComponent?.RevokeObjectUrlAsync(oldSrc);
+					cropperComponent?.Destroy();
+					cropperComponent?.RevokeObjectUrlAsync(oldSrc);
 
-                    StateHasChanged();
-                }
-            }
-        }
+					StateHasChanged();
+				}
+			}
+		}
 
-        public void OnErrorLoadImageEvent(Microsoft.AspNetCore.Components.Web.ErrorEventArgs errorEventArgs)
-        {
-            IsErrorLoadImage = true;
-            Destroy();
-            StateHasChanged();
-        }
+		public void OnErrorLoadImageEvent(Microsoft.AspNetCore.Components.Web.ErrorEventArgs errorEventArgs)
+		{
+			Destroy();
+			StateHasChanged();
+		}
 
-        private void Destroy()
-        {
-            cropperComponent?.Destroy();
-            cropperComponent?.RevokeObjectUrlAsync(src);
-        }
+		private void Destroy()
+		{
+			cropperComponent?.Destroy();
+			cropperComponent?.RevokeObjectUrlAsync(src);
+		}
 
-        public async Task SaveAvatar()
-        {
-            GetCroppedCanvasOptions getCroppedCanvasOptions = new GetCroppedCanvasOptions
-            {
-                MaxHeight = 4096,
-                MaxWidth = 4096,
-                ImageSmoothingQuality = ImageSmoothingQuality.High.ToEnumString()
-            };
+		public async Task SaveAvatar()
+		{
+			GetCroppedCanvasOptions getCroppedCanvasOptions = new GetCroppedCanvasOptions
+			{
+				MaxHeight = 4096,
+				MaxWidth = 4096,
+				ImageSmoothingQuality = ImageSmoothingQuality.High.ToEnumString()
+			};
 
-            var croppedCanvasDataURL = await cropperComponent!.GetCroppedCanvasDataURLAsync(getCroppedCanvasOptions);
-            var fileName = SaveDataUrlToFile(croppedCanvasDataURL, Path.Combine(AppContext.BaseDirectory, "Avatars"));
+			var croppedCanvasDataURL = await cropperComponent!.GetCroppedCanvasDataURLAsync(getCroppedCanvasOptions);
+			var fileName = SaveDataUrlToFile(croppedCanvasDataURL, Path.Combine(AppContext.BaseDirectory, "Avatars"));
 
-            var userState = await _stateProvider.GetAuthenticationStateAsync();
-            using var context = await _dbFactory.CreateDbContextAsync();
-            var user = context.Users.Find(userState.User.GetUserId());
-            if (user != null)
-            {
-                user.Avatar = fileName;
-                await context.SaveChangesAsync();
-                _snackbarService.Add("头像设置成功！", Severity.Success);
+			var userState = await _stateProvider.GetAuthenticationStateAsync();
+			using var context = await _dbFactory.CreateDbContextAsync();
+			var user = context.Users.Find(userState.User.GetUserId());
+			if (user != null)
+			{
+				var oldFileName = user.Avatar;
+				user.Avatar = fileName;
+				await context.SaveChangesAsync();
+				if (!string.IsNullOrEmpty(oldFileName))
+				{
+					File.Delete(Path.Combine(AppContext.BaseDirectory, "Avatars", oldFileName));
+				}
+				_snackbarService.Add("头像设置成功！", Severity.Success);
 			}
 			MudDialog?.Close(DialogResult.Ok(true));
 		}
 
-        public string SaveDataUrlToFile(string dataUrl, string savePath)
-        {
-            if (!Directory.Exists(savePath))
-            {
-                Directory.CreateDirectory(savePath);
-            }
+		private string SaveDataUrlToFile(string dataUrl, string savePath)
+		{
+			if (!Directory.Exists(savePath))
+			{
+				Directory.CreateDirectory(savePath);
+			}
 
-            var matchGroups = Regex.Match(dataUrl, @"^data:((?<type>[\w\/]+))?;base64,(?<data>.+)$").Groups;
-            var base64Data = matchGroups["data"].Value;
-            var binData = Convert.FromBase64String(base64Data);
+			var matchGroups = Regex.Match(dataUrl, @"^data:((?<type>[\w\/]+))?;base64,(?<data>.+)$").Groups;
+			var base64Data = matchGroups["data"].Value;
+			var binData = Convert.FromBase64String(base64Data);
 
-            var endfix = matchGroups["type"].Value switch
-            {
-                "image/png" => ".png",
-                "image/jpeg" => ".jpeg",
-                "image/gif" => ".gif",
-                _ => ".jpg"
-            };
+			var endfix = matchGroups["type"].Value switch
+			{
+				"image/png" => ".png",
+				"image/jpeg" => ".jpeg",
+				"image/gif" => ".gif",
+				_ => ".jpg"
+			};
 
-            var newFileName = Guid.NewGuid().ToString("N") + endfix;
-            File.WriteAllBytes(Path.Combine(savePath, newFileName), binData);
-            return newFileName;
-        }
-    }
+			var newFileName = Guid.NewGuid().ToString("N") + endfix;
+			File.WriteAllBytes(Path.Combine(savePath, newFileName), binData);
+			return newFileName;
+		}
+	}
 }
