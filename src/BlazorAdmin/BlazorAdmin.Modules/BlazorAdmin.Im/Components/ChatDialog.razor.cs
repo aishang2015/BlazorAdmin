eea0414eb6c346c256de0227dae3271578db6d06
aishang2015 <zhang__tian__hua@126.com>
2023-12-21 16:18:04 +0800

    chat message com

--- src/BlazorAdmin/BlazorAdmin.Modules/BlazorAdmin.Im/Components/ChatDialog.razor.cs
+++ src/BlazorAdmin/BlazorAdmin.Modules/BlazorAdmin.Im/Components/ChatDialog.razor.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BlazorAdmin.Data.Entities;
+using FluentCodeServer.Core;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.JSInterop;
 using MudBlazor;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace BlazorAdmin.Im.Components
@@ -17,14 +21,145 @@ namespace BlazorAdmin.Im.Components
 
         private string _messageValue = string.Empty;
 
-        private object _selectedValue = 1;
+        private object? _selectedValue;
 
         private MudListItem? _selectedItem;
+
+        private List<ChannelModel> Channels = new List<ChannelModel>();
+
+        private List<MessageModel> MessageModels = new List<MessageModel>();
+
+        private List<UserModel> UserModels = new List<UserModel>();
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             var scrollModule = await _jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/BlazorAdmin.Im/js/scroll.js");
             await scrollModule.InvokeVoidAsync("scrollToBottom");
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            await InitialChannelList();
+
+            if (Channels.Count > 0)
+            {
+                _selectedValue = Channels.First();
+            }
+            await InitialMessage();
+        }
+
+        private async Task SelectedChannelChanged(object selectedValue)
+        {
+            _selectedValue = selectedValue;
+            await InitialMessage();
+        }
+
+        private async Task InitialChannelList()
+        {
+            var state = await _stateProvider.GetAuthenticationStateAsync();
+
+            using var context = await _dbFactory.CreateDbContextAsync();
+            var channels = (from c in context.ChatChannels
+                            join cm in context.ChatChannelMembers on c.Id equals cm.ChatChannelId
+                            where cm.MemberId == state.User.GetUserId()
+                            select c).AsNoTracking().ToList();
+
+            var channelIds = channels.Where(c => c.Type != (int)ChatChannelType.普通群聊).Select(c => c.Id);
+            var userList = (from cm in context.ChatChannelMembers
+                            join u in context.Users on cm.MemberId equals u.Id
+                            where channelIds.Contains(cm.MemberId) && cm.MemberId != state.User.GetUserId()
+                            select new
+                            {
+                                cm.ChatChannelId,
+                                u.RealName,
+                                u.Avatar,
+                            }).AsNoTracking().ToList();
+
+
+            foreach (var channel in channels)
+            {
+                var channelName = channel.Name;
+                var avatar = string.Empty;
+                if (channel.Type != (int)ChatChannelType.普通群聊)
+                {
+                    var findUser = userList.FirstOrDefault(u => u.ChatChannelId == channel.Id);
+                    channelName = findUser?.RealName;
+                    avatar = findUser?.Avatar;
+                }
+                Channels.Add(new ChannelModel
+                {
+                    Type = channel.Type,
+                    ChannelId = channel.Id,
+                    ChannelName = channelName,
+                    Avatar = avatar
+                });
+            }
+        }
+
+        private async Task InitialMessage()
+        {
+            var selectedChannel = _selectedValue as ChannelModel;
+            if (selectedChannel != null)
+            {
+                using var channelDbContext = _chatDbFactory.CreateDbContext(selectedChannel.ChannelId);
+                MessageModels = channelDbContext.ChatMessages.OrderBy(m => m.Id).Take(100).Select(c => new MessageModel
+                {
+                    MessageId = c.Id,
+                    SenderId = c.SenderId,
+                    CreatedTime = c.CreatedTime,
+                    Content = c.Content,
+                }).AsNoTracking().ToList();
+
+                using var adminDbContext = _dbFactory.CreateDbContext();
+                UserModels = (from cm in adminDbContext.ChatChannelMembers
+                              join u in adminDbContext.Users on cm.MemberId equals u.Id
+                              where cm.ChatChannelId == selectedChannel.ChannelId
+                              select new UserModel
+                              {
+                                  UserId = u.Id,
+                                  RealName = u.RealName,
+                                  Avatar = u.Avatar,
+                              }).AsNoTracking().ToList();
+
+                var state = await _stateProvider.GetAuthenticationStateAsync();
+                MessageModels.ForEach(m => m.IsCurrentUserSend = m.SenderId == state.User.GetUserId());
+            }
+        }
+
+        private record ChannelModel
+        {
+            public string? Avatar { get; set; }
+
+            public int Type { get; set; }
+
+            public int ChannelId { get; set; }
+
+            public string? ChannelName { get; set; }
+
+            public string? Caption { get; set; }
+        }
+
+        private record MessageModel
+        {
+            public int MessageId { get; set; }
+
+            public int SenderId { get; set; }
+
+            public DateTime CreatedTime { get; set; }
+
+            public string? Content { get; set; }
+
+            public bool IsCurrentUserSend { get; set; }
+        }
+
+        public record UserModel
+        {
+            public int UserId { get; set; }
+
+            public string RealName { get; set; } = null!;
+
+            public string? Avatar { get; set; }
         }
     }
 }
--- src/BlazorAdmin/BlazorAdmin.Web/Components/Pages/Login.razor.cs
+++ src/BlazorAdmin/BlazorAdmin.Web/Components/Pages/Login.razor.cs
@@ -10,84 +10,91 @@ using System.Security.Claims;
 
 namespace BlazorAdmin.Web.Components.Pages
 {
-	public partial class Login
-	{
-		private Dictionary<string, object> InputAttributes { get; set; } =
-			new Dictionary<string, object>()
-				{
-				   { "autocomplete", "new-password2" },
-				};
+    public partial class Login
+    {
+        private Dictionary<string, object> InputAttributes { get; set; } =
+            new Dictionary<string, object>()
+                {
+                   { "autocomplete", "new-password2" },
+                };
 
-		private bool ShowContent;
+        private bool ShowContent;
 
-		private bool IsLoading = false;
+        private bool IsLoading = false;
 
-		private LoginModel _loginModel = new();
+        private LoginModel _loginModel = new();
 
-		protected override async Task OnAfterRenderAsync(bool firstRender)
-		{
-			if (firstRender)
-			{
-				await _authService.SetCurrentUser();
-				var state = await _stateProvider.GetAuthenticationStateAsync();
-				if (state.User.Identity != null && state.User.Identity.IsAuthenticated)
-				{
-					_navManager.NavigateTo("/");
-				}
-				ShowContent = true;
-				StateHasChanged();
-			}
-		}
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await _authService.SetCurrentUser();
+                var state = await _stateProvider.GetAuthenticationStateAsync();
+                if (state.User.Identity != null && state.User.Identity.IsAuthenticated)
+                {
+                    _navManager.NavigateTo("/");
+                }
+                ShowContent = true;
+                StateHasChanged();
+            }
+        }
 
-		private async Task LoginSubmit()
-		{
-			IsLoading = true;
-			using var context = await _dbFactory.CreateDbContextAsync();
-			var user = context.Users.FirstOrDefault(u => u.Name == _loginModel.UserName &&
-				u.IsEnabled && !u.IsDeleted && !u.IsSpecial);
-			if (user == null || !HashHelper.VerifyPassword(user.PasswordHash, _loginModel!.Password!))
-			{
-				_snackbarService.Add("用户名或密码错误！", Severity.Error);
-				await RecordLogin(_loginModel.UserName!, false, context);
-				IsLoading = false;
-				return;
-			}
+        private async Task LoginSubmit()
+        {
+            IsLoading = true;
+            using var context = await _dbFactory.CreateDbContextAsync();
+            var user = context.Users.FirstOrDefault(u => u.Name == _loginModel.UserName &&
+                u.IsEnabled && !u.IsDeleted && !u.IsSpecial);
+            if (user == null || !HashHelper.VerifyPassword(user.PasswordHash, _loginModel!.Password!))
+            {
+                _snackbarService.Add("用户名或密码错误！", Severity.Error);
+                await RecordLogin(_loginModel.UserName!, false, context);
+                IsLoading = false;
+                return;
+            }
 
             var token = _jwtHelper.GenerateJwtToken(new List<Claim>() {
-				new Claim(ClaimConstant.UserId,user.Id.ToString()),
-				new Claim(ClaimConstant.UserName,user.Name)
-			});
+                new Claim(ClaimConstant.UserId,user.Id.ToString()),
+                new Claim(ClaimConstant.UserName,user.Name)
+            });
 
-			await _localStorage.SetAsync(CommonConstant.UserId, user.Id);
-			await _localStorage.SetAsync(CommonConstant.UserToken, token);
-			await _authService.SetCurrentUser();
-			await RecordLogin(_loginModel.UserName!, true, context);
-			_navManager.NavigateTo("/");
-		}
+            await _localStorage.SetAsync(CommonConstant.UserId, user.Id);
+            await _localStorage.SetAsync(CommonConstant.UserToken, token);
+            await _authService.SetCurrentUser();
+            await RecordLogin(_loginModel.UserName!, true, context);
+            _navManager.NavigateTo("/");
 
-		private async Task RecordLogin(string userName, bool isSucceed,
-			BlazorAdminDbContext context)
-		{
-			context.LoginLogs.Add(new LoginLog
-			{
-				UserName = userName,
-				IsSuccessd = isSucceed,
-				Time = DateTime.Now,
-				Ip = _httpAccessor.HttpContext?.Connection.RemoteIpAddress?.ToString(),
-				Agent = _httpAccessor.HttpContext?.Request.Headers["User-Agent"]
-			});
-			await context.SaveChangesAsync();
-		}
+            await ChannelHelper<ChatMessageModel>.Instance.Writer.WriteAsync(new ChatMessageModel
+            {
+                SenderId = 1,
+                Content = "欢迎回来！",
+                ReceiverId = user.Id
+            });
+        }
 
-		private record LoginModel
-		{
-			[Required(ErrorMessageResourceName = "Login_UserNameHelpText",
-				ErrorMessageResourceType = typeof(CusCulture))]
-			public string? UserName { get; set; }
+        private async Task RecordLogin(string userName, bool isSucceed,
+            BlazorAdminDbContext context)
+        {
+            context.LoginLogs.Add(new LoginLog
+            {
+                UserName = userName,
+                IsSuccessd = isSucceed,
+                Time = DateTime.Now,
+                Ip = _httpAccessor.HttpContext?.Connection.RemoteIpAddress?.ToString(),
+                Agent = _httpAccessor.HttpContext?.Request.Headers["User-Agent"]
+            });
+            await context.SaveChangesAsync();
+        }
 
-			[Required(ErrorMessageResourceName = "Login_PasswordHelpText",
-				ErrorMessageResourceType = typeof(CusCulture))]
-			public string? Password { get; set; }
-		}
-	}
+        private record LoginModel
+        {
+            [Required(ErrorMessageResourceName = "Login_UserNameHelpText",
+                ErrorMessageResourceType = typeof(CusCulture))]
+            public string? UserName { get; set; }
+
+            [Required(ErrorMessageResourceName = "Login_PasswordHelpText",
+                ErrorMessageResourceType = typeof(CusCulture))]
+            public string? Password { get; set; }
+        }
+    }
 }
