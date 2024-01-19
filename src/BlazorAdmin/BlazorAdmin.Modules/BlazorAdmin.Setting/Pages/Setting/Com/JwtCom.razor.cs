282b22334db06e6c052a485f9d8e7db13372621d
aishang2015 <zhang__tian__hua@126.com>
2024-01-19 15:08:22 +0800

    jwt config

--- src/BlazorAdmin/BlazorAdmin.Modules/BlazorAdmin.Setting/Pages/Setting/Com/JwtCom.razor.cs
+++ src/BlazorAdmin/BlazorAdmin.Modules/BlazorAdmin.Setting/Pages/Setting/Com/JwtCom.razor.cs
@@ -7,70 +7,70 @@ using System.Security.Cryptography;
 namespace BlazorAdmin.Setting.Pages.Setting.Com
 {
     public partial class JwtCom
-	{
-		private JwtConfig JwtConfigModel;
+    {
+        private JwtConfig JwtConfigModel;
 
-		protected override async Task OnInitializedAsync()
-		{
-			await base.OnInitializedAsync();
-			await GetSettingsAsync();
-		}
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            await GetSettingsAsync();
+        }
 
-		private Task GetSettingsAsync()
-		{
-			using var dbContext = _dbFactory.CreateDbContext();
-			JwtConfigModel = new JwtConfig
-			{
-				Issuer = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtIssue)!.Value,
-				Audience = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtAudience)!.Value,
-				ExpireMinutes = int.Parse(dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtExpireMinute)!.Value),
+        private Task GetSettingsAsync()
+        {
+            using var dbContext = _dbFactory.CreateDbContext();
+            JwtConfigModel = new JwtConfig
+            {
+                Issuer = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtIssue)!.Value,
+                Audience = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtAudience)!.Value,
+                ExpireMinutes = int.Parse(dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtExpireMinute)!.Value),
 				RsaPrivateKey = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtSigningRsaPrivateKey)!.Value,
-				RsaPublicKey = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtSigningRsaPublicKey)!.Value,
-			};
-			return Task.CompletedTask;
-		}
+                RsaPublicKey = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtSigningRsaPublicKey)!.Value,
+            };
+            return Task.CompletedTask;
+        }
 
-		private async Task GenerateNewRsa()
-		{
-			var options = new DialogOptions() { CloseButton = true, MaxWidth = MaxWidth.ExtraLarge, NoHeader = true };
-			var result = await _dialogService.Show<ConfirmUpdateRsa>(string.Empty, new DialogParameters(), options).Result;
-			if (!result.Canceled)
-			{
-				var rsa = RSA.Create();
-				JwtConfigModel.RsaPrivateKey = Convert.ToBase64String(rsa.ExportRSAPrivateKey());
-				JwtConfigModel.RsaPublicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey());
-			}
-		}
+        private async Task GenerateNewRsa()
+        {
+            var options = new DialogOptions() { CloseButton = true, MaxWidth = MaxWidth.ExtraLarge, NoHeader = true };
+            var result = await _dialogService.Show<ConfirmUpdateRsa>(string.Empty, new DialogParameters(), options).Result;
+            if (!result.Canceled)
+            {
+                var rsa = RSA.Create();
+                JwtConfigModel.RsaPrivateKey = Convert.ToBase64String(rsa.ExportRSAPrivateKey());
+                JwtConfigModel.RsaPublicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey());
+            }
+        }
 
-		private async Task JwtConfigSubmit()
-		{
-			using var dbContext = _dbFactory.CreateDbContext();
-			var issuer = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtIssue);
-			issuer.Value = JwtConfigModel.Issuer;
-			var audience = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtAudience);
-			audience.Value = JwtConfigModel.Audience;
-			var expireMinutes = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtExpireMinute);
-			expireMinutes.Value = JwtConfigModel.ExpireMinutes.ToString();
-			var rsaPrivateKey = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtSigningRsaPrivateKey);
-			rsaPrivateKey.Value = JwtConfigModel.RsaPrivateKey;
-			var rsaPublicKey = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtSigningRsaPublicKey);
-			rsaPublicKey.Value = JwtConfigModel.RsaPublicKey;
-			await dbContext.AuditSaveChangesAsync();
+        private async Task JwtConfigSubmit()
+        {
+            using var dbContext = _dbFactory.CreateDbContext();
+            var issuer = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtIssue);
+            issuer!.Value = JwtConfigModel.Issuer!;
+            var audience = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtAudience);
+            audience!.Value = JwtConfigModel.Audience!;
+            var expireMinutes = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtExpireMinute);
+            expireMinutes!.Value = JwtConfigModel.ExpireMinutes.ToString();
+            var rsaPrivateKey = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtSigningRsaPrivateKey);
+            rsaPrivateKey!.Value = JwtConfigModel.RsaPrivateKey!;
+            var rsaPublicKey = dbContext.Settings.FirstOrDefault(s => s.Key == JwtConstant.JwtSigningRsaPublicKey);
+            rsaPublicKey!.Value = JwtConfigModel.RsaPublicKey!;
+            await dbContext.AuditSaveChangesAsync();
 
-			_snackbarService.Add("保存成功！", Severity.Success);
-		}
+            _snackbarService.Add("保存成功！", Severity.Success);
+        }
 
-		private class JwtConfig
-		{
-			public required string Issuer { get; set; }
+        private class JwtConfig
+        {
+            public string? Issuer { get; set; }
 
-			public required string Audience { get; set; }
+            public string? Audience { get; set; }
 
-			public required string RsaPrivateKey { get; set; }
+            public string? RsaPrivateKey { get; set; }
 
-			public required string RsaPublicKey { get; set; }
+            public string? RsaPublicKey { get; set; }
 
-			public int ExpireMinutes { get; set; }
-		}
-	}
+            public int ExpireMinutes { get; set; }
+        }
+    }
 }
--- src/BlazorAdmin/BlazorAdmin.Modules/BlazorAdmin.Setting/Pages/Setting/SettingPage.razor.cs
+++ src/BlazorAdmin/BlazorAdmin.Modules/BlazorAdmin.Setting/Pages/Setting/SettingPage.razor.cs
@@ -3,26 +3,29 @@ using MudBlazor;
 
 namespace BlazorAdmin.Setting.Pages.Setting
 {
-	public partial class SettingPage
-	{
-		private MudListItem? SelectedItem;
+    public partial class SettingPage
+    {
+        private MudListItem? SelectedItem;
 
-		private object SelectedValue = "JWT配置";
+        private object SelectedValue = "JWT配置";
 
-		private List<string> SettingGroups = new()
-		{
-			"JWT配置"
-		};
+        private List<dynamic> SettingGroups = [
+            new
+            {
+                Name = "JWT配置",
+                Icon = Icons.Material.Filled.Security
+            }
+            ];
 
-		private Dictionary<string, Type> SettingComDic = new Dictionary<string, Type>
-		{
-			 {"JWT配置",typeof(JwtCom)}
-		};
+        private Dictionary<string, Type> SettingComDic = new Dictionary<string, Type>
+        {
+             {"JWT配置",typeof(JwtCom)}
+        };
 
-		protected override async Task OnInitializedAsync()
-		{
-			await base.OnInitializedAsync();
-		}
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
 
-	}
+    }
 }
