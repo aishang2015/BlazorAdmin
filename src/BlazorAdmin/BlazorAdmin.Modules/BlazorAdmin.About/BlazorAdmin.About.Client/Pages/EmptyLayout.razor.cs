9fa28c8e684bfd202e4caa1037d72794eaa57b85
aishang2015 <zhang__tian__hua@126.com>
2024-12-26 17:10:42 +0800

    a test page

---
+++ src/BlazorAdmin/BlazorAdmin.Modules/BlazorAdmin.About/BlazorAdmin.About.Client/Pages/EmptyLayout.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorAdmin.About.Client.Pages
+{
+    public partial class EmptyLayout
+    {
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+        }
+    }
+}
